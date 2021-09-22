712308dde2937aaecc214bfe907625c456e8a114
kennychr <kenny.christiansen@gmail.com>
2021-09-22 21:03:04 +0200

    Initial commit#2
    Cleaned up configs

--- addons/ieds/CfgMagazines.hpp
+++ addons/ieds/CfgMagazines.hpp
@@ -1,5 +1,4 @@
 class CfgMagazines {
-
 	class ATMine_Range_Mag;
 	class EODS_WaterContainer_PP_Mag: ATMine_Range_Mag {
 			model = QPATHTOF(meshes\WaterBomb.p3d);
@@ -9,13 +8,9 @@ class CfgMagazines {
 	        class ACE_Triggers {
 				SupportedTriggers[] = {"PressurePlate"};
 				class PressurePlate {
-					
 					digDistance = 0;
 					//pitch = 0;
 				};
-
         };
-
 	};
-
 };
--- addons/ieds/CfgVehicles.hpp
+++ addons/ieds/CfgVehicles.hpp
@@ -1,13 +1,6 @@
 class cfgVehicles {
-	
 	//ACE Explosives Framework Start
 	class ACE_Explosives_Place;
-	
-
-
-
-	
-
 	class EODS_WaterContainer_PP_Place: ACE_Explosives_Place {
 		displayName = "Water Container IED PressurePlate";
 	    mapSize = 1;
@@ -17,14 +10,10 @@ class cfgVehicles {
 		model = QPATHTOF(meshes\WaterBomb.p3d);
 		//vehicleClass = "EODS_ied_class";
 		picture = QPATHTOF(data\ied07_menu.paa);
-		
 		ACE_offset[] = {0,0,0};
 		//This or CfgAmmo?
 		class Eventhandlers	{
 			init = "_this animate ['Detonator_hide', 1];_this animate ['Battery_hide', 1];";
 		};
-
 	};
-
-	//ACE Explosives Framework End
 };
