2341af0aa2383ac5020d192136edde706c116dc4
kennychr <kenny.christiansen@gmail.com>
2021-09-25 19:46:20 +0200

    Overide ACE Defuse dialog
    Overide and alter the ACE Defuse dialog

--- addons/ieds/CfgVehicles.hpp
+++ addons/ieds/CfgVehicles.hpp
@@ -1,6 +1,53 @@
 class cfgVehicles {
 	//ACE Explosives Framework Start
 	class ACE_Explosives_Place;
+    //Trying to modify Defuse helper obj
+
+    class Items_base_F;
+    class ACE_DefuseObject: Items_base_F {
+    class ACE_Actions {
+        class ACE_Defuse {
+                displayName = "Interact";
+                condition = "";
+                statement = "";
+                icon = "a3\characters_f\data\ui\icon_expl_specialist_ca.paa";
+                class ACE_Org_Defuse {
+                    displayName = "Defuse Vanilla\Ace3";
+                    distance = 1;
+                    exceptions[] = {"isNotSwimming"};
+                    condition = "[_player, _target] call ace_explosives_fnc_canDefuse";
+                    statement = "[_player, _target] call ace_explosives_fnc_startDefuse;";
+                    icon = "\z\ace\addons\explosives\UI\Defuse_ca.paa";
+                };
+                class EODS_Survey {
+                    displayName = "Survey Explosive";
+                    distance = 1;
+                    exceptions[] = {"isNotSwimming"};
+                    condition = "[_player, _target] call ace_explosives_fnc_canDefuse";
+                    statement = QUOTE([ARR_2(_player,_target)] call FUNC(survey););
+                    icon = "a3\ui_f\data\map\diary\icons\diarylocatetask_ca.paa";
+                };
+                class EODS_Defuse_Battery {
+                    displayName = "Defuse Powersource";
+                    distance = 1;
+                    exceptions[] = {"isNotSwimming"};
+                    condition = "[_player, _target] call ace_explosives_fnc_canDefuse";
+                    statement = "hint 'test'";
+                    icon = "a3\weapons_f\data\ui\m_battery_ca.paa";
+                };
+                class EODS_Defuse_Detonator{
+                    displayName = "Defuse Detonator";
+                    distance = 1;
+                    exceptions[] = {"isNotSwimming"};
+                    condition = "[_player, _target] call ace_explosives_fnc_canDefuse";
+                    statement = "hint 'test'";
+                    icon = "a3\characters_f\data\ui\icon_expl_specialist_ca.paa";
+                };
+            };
+        };
+    };
+
+
 	class EODS_WaterContainer_PP_Place: ACE_Explosives_Place {
 		displayName = "Water Container IED PressurePlate";
 	    mapSize = 1;
@@ -12,8 +59,57 @@ class cfgVehicles {
 		picture = QPATHTOF(data\ied07_menu.paa);
 		ACE_offset[] = {0,0,0};
 		//This or CfgAmmo?
-		class Eventhandlers	{
-			init = "_this animate ['Detonator_hide', 1];_this animate ['Battery_hide', 1];";
+		class Eventhandlers	{};
+        	class HitPoints	{
+                class HitIed {
+                    armor = 1;
+                    material = -1;
+                    name = "Bomb";
+                    passThrough = 0;
+                };
 		};
+	};
+
+	class EODS_122mm_USSR_IED_RC_Place: ACE_Explosives_Place {
+		displayName = "122mm RCIED";
+	    mapSize = 1;
+		author = "Christiansen";
+		scope = 2;
+		model = QPATHTOF(meshes\122mm_USSR_IED_d.p3d);
+		//vehicleClass = "EODS_ied_class";
+		picture = QPATHTOF(data\ied07_menu.paa);
+		ACE_offset[] = {0,0,0};
+		//This or CfgAmmo?
+		class Eventhandlers	{};
+
+
+	};
+    class EODS_Ied_01_PP_Place: ACE_Explosives_Place {
+	displayName = "Ied 01 PP";
+	mapSize = 1;
+	author = "Christiansen";
+	scope = 2;
+	model = QPATHTOF(meshes\Ied_PP01.p3d);
+	//vehicleClass = "EODS_ied_class";
+	picture = QPATHTOF(data\ied07_menu.paa);
+	ACE_offset[] = {0,0,0};
+	//This or CfgAmmo?
+	class Eventhandlers	{};
+
+
+	};
+	class EODS_Ied_02_PP_Place: ACE_Explosives_Place {
+		displayName = "Ied 02 PP";
+	    mapSize = 1;
+		author = "Christiansen";
+		scope = 2;
+		model = QPATHTOF(meshes\Ied_PP02.p3d);
+		//vehicleClass = "EODS_ied_class";
+		picture = QPATHTOF(data\ied07_menu.paa);
+		ACE_offset[] = {0,0,0};
+		//This or CfgAmmo?
+		class Eventhandlers	{};
+
+
 	};
 };
